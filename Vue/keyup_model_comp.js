fa0aa98bb27c9ae0a2bf507f271b78ec0cbb032a
Neisser Sánchez <rko_619_fbi@hotmail.com>
2020-11-27 22:07:43 -0500

    Vue

--- Vue/keyup_model_comp.js
+++ Vue/keyup_model_comp.js
@@ -11,6 +11,50 @@ const app = new Vue({
             {nombre: 'Platano', cantidad: 0},
             {nombre: 'Manzana', cantidad: 8}
         ],
+        nuevaFruta: '',
+        total: 0   
+    },
+
+    methods: {
+        agregarFruta(){
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad: 0
+            });
+
+            this.nuevaFruta = '';
+        }
+    },
+
+    computed: {
+        sumarFrutas(){
+            this.total = 0;
+            for(fruta of this.frutas){
+                this.total = this.total + fruta.cantidad;
+            }
+            return this.total;
+        }
+    },
+})
+
+
+/* 
+
+Hasta aquí si damos click en agregar se agrega el contenido ingresado y se limpia pantalla,
+pero cuando presionamos enter no sucede nada, entonces vamos a la documentacion de vue y 
+buscamos keyup (https://vuejs.org/v2/guide/events.html#Key-Modifiers)
+
+const app = new Vue({
+
+    el: '#app',
+
+    data: {
+        titulo: 'Hola mundo con Vue',
+
+        frutas: [
+            {nombre: 'Pera', cantidad: 10},
+            {nombre: 'Platano', cantidad: 0},
+            {nombre: 'Manzana', cantidad: 8}
+        ],
         nuevaFruta: ''   
     },
 
@@ -25,8 +69,16 @@ const app = new Vue({
     }
 })
 
+-----------------------
+El v-model en el html puede ser transformado 
 
-/* 
+v-model.number
+
+
+COMPUTED:
+Es otra propiedad!
+SON ARREGLOS Y DENTRO PODEMOS CREAR LAS FUNCIONES Q NECESITEMOS
+
 
 
 */
