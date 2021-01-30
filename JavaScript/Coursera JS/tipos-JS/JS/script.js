05dbbbb2650311203dd67ffa7a3b80cebb644516
Neisser Sánchez <rko_619_fbi@hotmail.com>
2021-01-30 16:48:31 -0500

    JS

---
+++ JavaScript/Coursera JS/tipos-JS/JS/script.js
@@ -0,0 +1,15 @@
+// should be undefined
+var x;
+console.log(x);
+
+if (x == undefined) {
+    console.log("x is undefined");
+}
+
+x = 5;
+if (x == undefined) {
+    console.log("x is undefined");
+}
+else {
+    console.log("x has been defined");
+}
